52fdc05790308ef4a191e3a9656d277a2eb41a01
David Tucker <dtucke03@dcs.bbk.ac.uk>
2017-04-16 23:47:47 +0100

    4.  Instead of newing  up the various sensors when we call pollSensors() , we want to pass a collection of sensors in to the ControlUnit somehow.  However, we don’t want to pass the sensors in when we are polling.  When we poll sensors, the control unit should be configured with all of the required sensors.  (Hint:  Dependency Inversion Principle).

--- Exercises/week02/src/main/scala/App.scala
+++ Exercises/week02/src/main/scala/App.scala
@@ -1,13 +1,18 @@
 import java.io.IOException
 import java.util.Scanner
 
+import scala.collection.mutable.ListBuffer
+
 object App {
   private val EXIT: String = "exit"
   private val POLL: String = "poll"
 
   @throws[IOException]
   def main(args: Array[String]) {
-    val controlUnit: ControlUnit = new ControlUnit
+    val sensors = new ListBuffer[Sensor]()
+    sensors += new FireSensor()
+    sensors += new SmokeSensor()
+    val controlUnit: ControlUnit = new ControlUnit(sensors)
     val scanner: Scanner = new Scanner(System.in)
     var input: String = ""
     while (input != EXIT) {
--- Exercises/week02/src/main/scala/ControlUnit.scala
+++ Exercises/week02/src/main/scala/ControlUnit.scala
@@ -1,10 +1,7 @@
 import scala.collection.mutable.ListBuffer
 
-class ControlUnit {
+class ControlUnit(val sensors: ListBuffer[Sensor]) {
   def pollSensors() {
-    val sensors = new ListBuffer[Sensor]()
-    sensors += new FireSensor()
-    sensors += new SmokeSensor()
     for (sensor <- sensors) {
       if (sensor.isTriggered) {
         System.out.println("A " + sensor.getSensorType + " sensor was triggered at " + sensor.getLocation)
