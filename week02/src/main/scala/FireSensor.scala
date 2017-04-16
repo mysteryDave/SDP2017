e1d69120b2a785592d2c24f25f178f697a1e6bbc
David Tucker <dtucke03@dcs.bbk.ac.uk>
2017-04-16 21:05:14 +0100

    Initial Commit (Exercise 2 - SOLID)

---
+++ week02/src/main/scala/FireSensor.scala
@@ -0,0 +1,11 @@
+import scala.util.Random
+
+class FireSensor extends Sensor {
+  override def isTriggered: Boolean = false
+
+  override def getLocation: String = null
+
+  override def getSensorType: String = null
+
+  override def getBatteryPercentage: Double = -1
+}
---
+++ week02/src/test/scala/FireSensorTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class FireSensorTest {
+
+  @Test
+  public void testThatIsTriggeredReturnsFalse() {
+    FireSensor sensor = new FireSensor();
+    boolean isTriggered = sensor.isTriggered();
+    assertEquals(false, isTriggered);
+  }
+
+  @Test
+  public void testThatGetLocationReturnsNull() {
+    FireSensor sensor = new FireSensor();
+    String location = sensor.getLocation();
+    assertEquals(null, location);
+  }
+
+  @Test
+  public void testThatGetSensorTypeReturnsNull() {
+    FireSensor sensor = new FireSensor();
+    String sensorType = sensor.getSensorType();
+    assertEquals(null, sensorType);
+  }
+
+  @Test
+  public void testThatGetBatteryPercentageReturnsNegativeOne() {
+    FireSensor sensor = new FireSensor();
+    double batteryPercentage = sensor.getBatteryPercentage();
+    assertEquals(-1.0, batteryPercentage, 0.01);
+  }
+
+  @Test
+  public void testThatAlarmRaisedPercentageIsFive() {
+    int nNumberOfTests = 10000;
+    int nTriggered = 0;
+    FireSensor sensor = new FireSensor();
+    for (int nTest = 1; nTest<=nNumberOfTests; nTest++) {
+      if(sensor.isTriggered()) nTriggered++;
+    }
+    double dAlarmRaisedPercent = 100.0 * (double)nTriggered / (double)nNumberOfTests;
+    assertEquals(5.0, dAlarmRaisedPercent, 0.5);
+  }
+}
