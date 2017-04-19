59586fab8e133ebc6340f9f8ed1e780e70f67386
David Tucker <dtucke03@dcs.bbk.ac.uk>
2017-04-19 22:31:22 +0100

    Implemented: 2.  (b) What happens when we define a companion object for a case class? Take our Person class and turn it into a case class. Make sure you still have the companion object with the alternate apply method as well.

--- Exercises/week05/src/main/scala/Counter.scala
+++ Exercises/week05/src/main/scala/Counter.scala
@@ -1,7 +1,13 @@
 /**
   * Created by David Tucker on 18/04/2017.
   */
+
+class Adder(amount: Int) {
+  def add(in: Int) = in + amount
+}
+
 case class Counter(var count: Int = 0) {
   def inc(nStep: Int = 1): Counter = copy(count + nStep)
   def dec(nStep: Int = 1): Counter = copy(count - nStep)
+  def adjust(nFunction: Adder): Counter = copy(nFunction.add(count))
 }
