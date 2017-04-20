5c1da38ab31de16558b49120d1a9cf8c5ddb268b
David Tucker <dtucke03@dcs.bbk.ac.uk>
2017-04-20 03:58:08 +0100

    Coursework One: Initial Commit

--- Exercises/week05/src/main/scala/films/Film.scala
+++ Exercises/week05/src/main/scala/films/Film.scala
@@ -4,18 +4,32 @@ package films
   * Created by David Tucker on 19/04/2017.
   */
 
-class Director(val firstName: String, val lastName: String, val yearOfBirth: Int) {
+case class Director(firstName: String, lastName: String, yearOfBirth: Int) {
   val name: String = firstName + " " + lastName
 }
 
-class Film(val name: String,
-           val yearOfRelease: Int,
-           val imdbRating: Double,
-           val director: Director) {
+object Director {
+  /*def apply(firstName: String, lastName: String, yearOfBirth: Int): Director = new Director(firstName, lastName, yearOfBirth)*/
+  def older(one: Director, other: Director) = if (one.yearOfBirth < other.yearOfBirth) one else other
+}
+
+case class Film(name: String,
+           yearOfRelease: Int,
+           imdbRating: Double,
+           director: Director) {
   def directorsAge: Int = yearOfRelease - director.yearOfBirth
   def isDirectedBy(wasIt: Director): Boolean = wasIt.name.equals(director.name) && wasIt.yearOfBirth == director.yearOfBirth //would wasIt == director work? No - Only if system exclusively uses same instance.
-  def copy(name: String = this.name,
+  /*def copy(name: String = this.name,
            yearOfRelease: Int = this.yearOfRelease,
            imdbRating: Double = this.imdbRating,
-           director: Director = this.director) = new Film(name, yearOfRelease, imdbRating, director)
+           director: Director = this.director) = new Film(name, yearOfRelease, imdbRating, director)*/
+}
+
+object Film {
+  /*def apply(name: String,
+            yearOfRelease: Int,
+            imdbRating: Double,
+            director: Director): Film = new Film(name, yearOfRelease, imdbRating, director)*/
+  def highestRating(one: Film, other: Film): Film = if (one.imdbRating > other.imdbRating) one else other
+  def oldestDirectorAtTheTime(one: Film, other: Film): Director = if (one.directorsAge > other.directorsAge) one.director else other.director
 }
