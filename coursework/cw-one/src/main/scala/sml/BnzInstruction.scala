30182f488e3039f03eca485a52cfb1dfbc52d998
mysteryDave <mystery.dave@yahoo.com>
2017-04-22 23:33:46 +0100

    Added bnz instruction Updated Translator for instructions

---
+++ coursework/cw-one/src/main/scala/sml/BnzInstruction.scala
@@ -0,0 +1,27 @@
+package sml
+
+/**
+  * Created by DT on 22/04/2017.
+  */
+
+case class BnzInstruction(label: String, opcode: String, register: Int, nextInstr: String) extends Instruction(label, opcode) {
+
+  override def execute(m: Machine): Unit = {
+    val value = m.regs(register)
+    if (value != 0) {
+      var counter = 0
+      while (!nextInstr.equals(m.labels(counter)) && counter < m.prog.length) counter = counter + 1
+      if (counter >= m.prog.length) throw new NoSuchElementException("Label: " + label + " tries to jump to instruction labelled: " + nextInstr + ". Cannot find " + nextInstr + ".")
+      else m.pc = counter
+    }
+  }
+
+  override def toString(): String = {
+    super.toString + " check register " + register + ", jump to instruction: " + nextInstr + "\n"
+  }
+}
+
+object BnzInstruction {
+  def apply(label: String, register: Int, nextInstr: String) =
+    new BnzInstruction(label, "bnz", register, nextInstr: String)
+}
--- coursework/cw-one/src/main/scala/sml/Translator.scala
+++ coursework/cw-one/src/main/scala/sml/Translator.scala
@@ -35,6 +35,8 @@ class Translator(fileName: String) {
             program = program :+ DivInstruction(fields(0), fields(2).toInt, fields(3).toInt, fields(4).toInt)
           case LIN =>
             program = program :+ LinInstruction(fields(0), fields(2).toInt, fields(3).toInt)
+          case BNZ =>
+            program = program :+ BnzInstruction(fields(0), fields(2).toInt, fields(3))
           case OUT =>
             program = program :+ OutInstruction(fields(0), fields(2).toInt)
           case x =>
